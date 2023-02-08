2a26792b724108d93e06c7c6b95051c89dbde56d
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-08 10:03:32 -0600

    Initial commit - add initial node and linked list classes

---
+++ script.js
@@ -0,0 +1,14 @@
+// LinkedList class 
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
+
+//Node class
+class Node {
+    constructor(value) {
+        this.value = null;
+        this.nextNode = null;
+    }
+}
