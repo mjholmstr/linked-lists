f98df9370c0e28f2df274b20a07503bc794091a8
Matt Holmstrom <mjholmstr@gmail.com>
2023-02-08 10:35:47 -0600

    Add append method and test case

--- script.js
+++ script.js
@@ -3,12 +3,34 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    append(value) {
+
+        if (this.head === null) {
+            this.head = new Node(value);
+        } else {
+            //set tail
+            let tail = this.head;
+            //cycle through node tails until next is null
+            while (tail.nextNode != null) {
+                tail = tail.nextNode;
+            }
+            //once at end node, append newNode as next
+            tail.nextNode = new Node(value);
+        }
+    }
 }
+
 
 //Node class
 class Node {
     constructor(value) {
-        this.value = null;
+        this.value = value;
         this.nextNode = null;
     }
 }
+
+const testList = new LinkedList();
+testList.append('test 1');
+testList.append('test 2');
+console.log(testList);
